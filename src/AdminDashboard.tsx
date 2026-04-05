4ba5123d88b55d2ca744567584b2be307ebfc0ab
yaseen177 <yaseen.hussain18@outlook.com>
2026-04-05 20:54:50 +0100

    fix

--- src/AdminDashboard.tsx
+++ src/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { AdminMapper } from './App';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid } from 'recharts';
 
