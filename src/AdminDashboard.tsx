b310d77322ee4cdb2ab49180cf6a8e4744f09ebf
yaseen177 <yaseen.hussain18@outlook.com>
2026-04-05 20:44:57 +0100

    bug ficx

--- src/AdminDashboard.tsx
+++ src/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { AdminMapper } from './App'; // Imports your secure mapping tool!
 
 export default function AdminDashboard() {
